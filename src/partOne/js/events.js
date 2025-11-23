8e99cce7369f8af12f15c900166fe4c468d91fc3
Leonela88 <rivasaela@gmail.com>
2025-11-23 14:27:53 +0100

    Add functions and an event to the button.test ok

--- src/partOne/js/events.js
+++ src/partOne/js/events.js
@@ -1,4 +1,6 @@
 import { changeStyles } from "./partOne.js";
 
+const buttonElement = document.querySelector(".button");
 
+buttonElement.addEventListener('click', changeStyles);
 //Aquí codificaremos el evento que disparará el botón "Click me"
--- src/partOne/js/partOne.js
+++ src/partOne/js/partOne.js
@@ -1,15 +1,22 @@
 //I parte, cambiar color y texto a darkblue
 
 function changeColor(){
-    //Escribe tu código aquí
+    const backgroundElement = document.getElementById("background");
+if(backgroundElement){
+  backgroundElement.style.backgroundColor ="darkblue";
+}
 };
 
 function changeText(){
-    //Escribe tu código aquí
+    const textElement =document.getElementById("text");
+    if (textElement){
+   textElement.innerHTML="darkblue";
+    }
 }; 
 
 function changeStyles(){
-    //Escribe tu código aquí
+    changeColor();
+    changeText();
 };
 
 export {changeColor, changeText, changeStyles}
